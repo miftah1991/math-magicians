ef3f9e20cd493bdc7485aef6bc5e00a1848ffb7f
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-03-25 00:02:32 +0430

    Test complete for Header, Home and Quote

---
+++ src/__tests__/logic/Quote.test.js
@@ -0,0 +1,7 @@
+import renderer from 'react-test-renderer';
+import Quote from '../../pages/Quote';
+
+it('renders correctly', () => {
+  const tree = renderer.create(<Quote />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
